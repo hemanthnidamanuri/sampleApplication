5cb47361268fa8c0c54ce5efc142b3119c18eb20
hemanthnidamanuri <hemanthnidamanuri89@gmail.com>
2019-02-15 19:06:13 +0530

    updated with paths

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { SeperateviewComponent } from './seperateview/seperateview.component';
+import { StudentComponent } from './student/student.component';
 
 
 export const routes: Routes = [
-
-    {path: 'seperateview', component: SeperateviewComponent}
+    {path: 'student', component: StudentComponent},
+    {path: 'seperateview/:id/:name/:phone/:colg',  component: SeperateviewComponent , pathMatch: 'full'}
   ];
 
 
